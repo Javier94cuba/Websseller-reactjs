b232d3d222abb35ca702f1a231c6f47901be60f0
Javier94cuba <jlopezhdez94@gmail.com>
2022-02-14 05:07:24 -0400

    formateando con prettier

--- src/componentes_heredados/obj_portafolio.js
+++ src/componentes_heredados/obj_portafolio.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Obj_Portafolio(props){
     return(
-        <article>
+        <article className="articulo_portafolio">
           <div className={props.image}></div>
           <p> {props.nombre}<br /> {props.info} </p>
         </article>
